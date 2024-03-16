d0ca08fc642032b5788d51fe55a304f264640d74
AC <abascharlesbenard@gmail.com>
2024-03-16 09:31:31 +0300

    My first react app

---
+++ components/Hello.js
@@ -0,0 +1,13 @@
+const name = "Abas";
+const displayMessage = () => {
+    return 'I need help';
+}
+function Hello (){
+    return <h1>The message is:  {displayMessage()}</h1>
+}
+
+
+// Using es6 syntax
+// const Hello = () => <h1>Hello There</h1>
+
+export default Hello;
